fc281c4adcd33c3b15280a3b2907723b56cd474c
Dev Karan Singh <dev1604karan@gmail.com>
2024-12-29 18:33:21 +0530

    ISAR notes app done

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:device_storage_isar/models/notes.dart';
+import 'package:device_storage_isar/models/notes_database.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -13,8 +16,15 @@ class _NotesPageState extends State<NotesPage> {
   //text controller to access what the user typed
   final textController = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+    //fetch notes
+    readNotes();
+  }
+
   //create a note
-  void createNote() {
+  void createNote(VoidCallback onNoteAdded) {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -25,12 +35,28 @@ class _NotesPageState extends State<NotesPage> {
           ),
         ),
         actions: [
+          //CANCEL
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              //pop the dialog
+              Navigator.of(context).pop();
+            },
             child: Text("Cancel"),
           ),
+          //SAVE
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<NotesDatabase>().addNote(textController.text);
+
+              setState(() {
+                textController.clear();
+              });
+
+              onNoteAdded();
+
+              //pop the dialog
+              Navigator.of(context).pop();
+            },
             child: Text("Save"),
           ),
         ],
@@ -39,20 +65,96 @@ class _NotesPageState extends State<NotesPage> {
   }
 
   //read a note
+  void readNotes() {
+    context.read<NotesDatabase>().fetchNotes();
+  }
 
   //update a note
+  void updateNotes(Notes note) {
+    textController.text = note.text;
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Update Note"),
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          //cancel button
+          MaterialButton(
+            child: Text("Cancel"),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+
+          //update button
+          MaterialButton(
+            child: Text("Update"),
+            onPressed: () {
+              //update note in db
+              context
+                  .read<NotesDatabase>()
+                  .updateNote(note.id, textController.text);
+
+              textController.clear();
+
+              //close dialog
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   //delete a note
+  void deleteNotes(int id) {
+    context.read<NotesDatabase>().deleteNotes(id);
+  }
 
   @override
   Widget build(BuildContext context) {
+    //notes database
+    final notesDatabase = context.watch<NotesDatabase>();
+
+    //current notes
+    List<Notes> currentNotes = notesDatabase.currentNotes;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Notes"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: createNote,
+        onPressed: () {
+          createNote(() {
+            setState(() {}); // Rebuild the ListView to show the new note
+          });
+        },
         child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: currentNotes.length,
+        itemBuilder: (context, index) {
+          //get individual note
+          final note = currentNotes[index];
+
+          //list tile UI
+          return ListTile(
+            title: Text(note.text),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //edit button
+                IconButton(
+                    onPressed: () => updateNotes(note), icon: Icon(Icons.edit)),
+
+                //delete button
+                IconButton(
+                    onPressed: () => deleteNotes(note.id),
+                    icon: Icon(Icons.delete)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
